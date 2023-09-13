32f572c5b25b9f862c6d863ed0e101c08dfc9e41
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-09-13 22:33:26 +0900

    feat: レスポンスのログ追加

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -5,9 +5,28 @@ import (
 	"net/http"
 )
 
+type resLoggingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+func NewResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
+	return &resLoggingWriter{ResponseWriter: w, code: http.StatusOK}
+}
+
+func (rsw *resLoggingWriter) WriteHeader(code int) {
+	rsw.code = code
+	rsw.ResponseWriter.WriteHeader(code)
+}
+
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		log.Println(req.RequestURI, req.Method)
+
+		rlw := NewResLoggingWriter(w)
+
 		next.ServeHTTP(w, req)
+
+		log.Println("res: ", rlw.code)
 	})
 }
