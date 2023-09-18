5d2e8efb2f11a4408a226932035df24479546d8e
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-09-18 14:22:43 +0900

    update: db構成変更

--- models/models.go
+++ models/models.go
@@ -5,6 +5,8 @@ import "time"
 type Comment struct {
 	CommentID int       `json:"comment_id"`
 	ArticleID int       `json:"article_id"`
+	UserID    int       `json:"user_id"`
+	UserName  string    `json:"user_name"`
 	Message   string    `json:"message"`
 	CreatedAt time.Time `json:"created_at"`
 }
@@ -13,8 +15,23 @@ type Article struct {
 	ID          int       `json:"id"`
 	Title       string    `json:"title"`
 	Contents    string    `json:"contents"`
+	UserID      int       `json:"user_id"`
 	UserName    string    `json:"user_name"`
 	NiceNum     int       `json:"nice"`
 	CommentList []Comment `json:"comments"`
 	CreatedAt   time.Time `json:"created_at"`
+}
+
+type User struct {
+	UserID    int       `json:"user_id"`
+	GoogleID  string    `json:"google_id"`
+	UserName  string    `json:"user_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type Nice struct {
+	NiceID    int `json:"nice_id"`
+	UserID    int `json:"user_id"`
+	ArticleID int `json:"article_id"`
 }
