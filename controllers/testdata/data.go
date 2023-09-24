08390714d14530920ab0f7338446fe1cbb3830da
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-09-24 13:56:14 +0900

    fix: modedata

--- controllers/testdata/data.go
+++ controllers/testdata/data.go
@@ -7,7 +7,8 @@ var articleTestData = []models.Article{
 		ID:          1,
 		Title:       "firstPost",
 		Contents:    "This is my first blog",
-		UserName:    "saki",
+		UserID:      1,
+		UserName:    "naoki",
 		NiceNum:     2,
 		CommentList: commentTestData,
 	},
@@ -15,7 +16,8 @@ var articleTestData = []models.Article{
 		ID:       2,
 		Title:    "2nd",
 		Contents: "Second blog post",
-		UserName: "saki",
+		UserID:   1,
+		UserName: "naoki",
 		NiceNum:  4,
 	},
 }
@@ -24,11 +26,15 @@ var commentTestData = []models.Comment{
 	models.Comment{
 		CommentID: 1,
 		ArticleID: 1,
+		UserID:    1,
+		UserName:  "naoki",
 		Message:   "1st comment yeah",
 	},
 	models.Comment{
 		CommentID: 2,
 		ArticleID: 1,
+		UserID:    1,
+		UserName:  "naoki",
 		Message:   "welcome",
 	},
 }
