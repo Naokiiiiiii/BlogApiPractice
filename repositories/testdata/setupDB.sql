9f166e9e21abd800bd514abbd10f708e473483ce
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-09-24 17:41:04 +0900

    fix: testSetUp修正

--- repositories/testdata/setupDB.sql
+++ repositories/testdata/setupDB.sql
@@ -26,22 +26,22 @@ create table if not exists comments (
 );
 
 create table if not exists nices (
-  nice_id integer unsigned auto_increment
-  article_id integer unsigned not null
-  user_id integer unsigned not null
-  created_at datetime
-  foreign key (article_id) references articles(article_id)
+  nice_id integer unsigned auto_increment primary key,
+  article_id integer unsigned not null,
+  user_id integer unsigned not null,
+  created_at datetime,
+  foreign key (article_id) references articles(article_id),
   foreign key (user_id) references users(user_id)
 );
 
-insert into users (username, email, created_at) values ('naoki', 'exsample@gmail.com', now())
+insert into users (username, email, created_at) values ('naoki', 'exsample@gmail.com', now());
 
 insert into articles (title, contents, user_id, created_at) values ('firstPost', 'This is my first blog', 1, now()); 
 
-insert into articles (title, contents, user_id, created_at) values ('firstPost', 'This is my second blog', 1, now()); 
+insert into articles (title, contents, user_id, created_at) values ('secondPost', 'This is my second blog', 1, now()); 
 
 insert into comments (article_id, user_id, message, created_at) values (1, 1, '1st comment yeah', now());
 
 insert into comments (article_id, user_id, message, created_at) values (1, 1, '2nd comment yeah', now());
 
-insert into nices (article_id, user_id, created_at) values (1, 1, now())
+insert into nices (article_id, user_id, created_at) values (1, 1, now());
