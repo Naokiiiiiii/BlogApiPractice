67149b6613f558a0d5f3571db228c0d9fd8590d2
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-10-08 14:40:16 +0900

    feat: google認証をcallbackまで実装

--- api/router.go
+++ api/router.go
@@ -1,14 +1,19 @@
 package api
 
 import (
+	"context"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/Naokiiiiiii/BlogApiPractice/api/middlewares"
 	"github.com/Naokiiiiiii/BlogApiPractice/controllers"
 	"github.com/Naokiiiiiii/BlogApiPractice/services"
 	"github.com/gorilla/mux"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 func NewRouter(db *sql.DB) *mux.Router {
@@ -19,6 +24,7 @@ func NewRouter(db *sql.DB) *mux.Router {
 	r := mux.NewRouter()
 
 	// 認証が必要ないAPI
+	r.HandleFunc("/login", handleGoogleLogin)
 	r.HandleFunc("/callback", handleGoogleCallback)
 
 	// 認証が必要なAPI
@@ -35,7 +41,48 @@ func NewRouter(db *sql.DB) *mux.Router {
 	return r
 }
 
+func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
+	config := oauth2.Config{
+		ClientID:     os.Getenv("GOOGLE_CLIANT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIANT_SECRET"),
+		RedirectURL:  "http://localhost:8080/callback",
+		Endpoint:     google.Endpoint,
+		Scopes:       []string{"profile", "email"},
+	}
+	url := config.AuthCodeURL("", oauth2.AccessTypeOffline)
+	http.Redirect(w, r, url, http.StatusFound)
+}
+
 func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
+	config := oauth2.Config{
+		ClientID:     os.Getenv("GOOGLE_CLIANT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIANT_SECRET"),
+		RedirectURL:  "http://localhost:8080/callback",
+		Endpoint:     google.Endpoint,
+		Scopes:       []string{"profile", "email"},
+	}
+
 	code := r.URL.Query().Get("code")
-	fmt.Println("redirect", code)
+	token, err := config.Exchange(context.Background(), code)
+	if err != nil {
+		http.Error(w, "Failed to exchange code for token", http.StatusInternalServerError)
+		return
+	}
+
+	client := config.Client(context.Background(), token)
+	response, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
+	if err != nil {
+		http.Error(w, "Failed to get user info", http.StatusInternalServerError)
+		return
+	}
+	defer response.Body.Close()
+
+	var userInfo map[string]interface{}
+	err = json.NewDecoder(response.Body).Decode(&userInfo)
+	if err != nil {
+		http.Error(w, "Failed to decode user info", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("userinfo", userInfo)
 }
