9cc8b1474fb0637a82e314909e4392ba092e1720
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-10-09 16:31:38 +0900

    feat: controllerを使うよう修正

--- api/router.go
+++ api/router.go
@@ -1,19 +1,13 @@
 package api
 
 import (
-	"context"
 	"database/sql"
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/Naokiiiiiii/BlogApiPractice/api/middlewares"
 	"github.com/Naokiiiiiii/BlogApiPractice/controllers"
 	"github.com/Naokiiiiiii/BlogApiPractice/services"
 	"github.com/gorilla/mux"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
 )
 
 func NewRouter(db *sql.DB) *mux.Router {
@@ -21,11 +15,12 @@ func NewRouter(db *sql.DB) *mux.Router {
 	aCon := controllers.NewArticleController(ser)
 	cCon := controllers.NewCommentController(ser)
 	nCon := controllers.NewNiceController(ser)
+	uCon := controllers.NewUserController(ser)
 	r := mux.NewRouter()
 
 	// 認証が必要ないAPI
-	r.HandleFunc("/login", handleGoogleLogin)
-	r.HandleFunc("/callback", handleGoogleCallback)
+	r.HandleFunc("/login", uCon.GoogleLoginHandler)
+	r.HandleFunc("/callback", uCon.GoogleCallbackHandler)
 
 	// 認証が必要なAPI
 	authRequired := r.PathPrefix("/").Subrouter()
@@ -38,50 +33,4 @@ func NewRouter(db *sql.DB) *mux.Router {
 	authRequired.HandleFunc("/comment", cCon.PostCommentHandler).Methods(http.MethodPost)
 
 	return r
-}
-
-func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
-	config := oauth2.Config{
-		ClientID:     os.Getenv("GOOGLE_CLIANT_ID"),
-		ClientSecret: os.Getenv("GOOGLE_CLIANT_SECRET"),
-		RedirectURL:  "http://localhost:8080/callback",
-		Endpoint:     google.Endpoint,
-		Scopes:       []string{"profile", "email"},
-	}
-	url := config.AuthCodeURL("", oauth2.AccessTypeOffline)
-	http.Redirect(w, r, url, http.StatusFound)
-}
-
-func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
-	config := oauth2.Config{
-		ClientID:     os.Getenv("GOOGLE_CLIANT_ID"),
-		ClientSecret: os.Getenv("GOOGLE_CLIANT_SECRET"),
-		RedirectURL:  "http://localhost:8080/callback",
-		Endpoint:     google.Endpoint,
-		Scopes:       []string{"profile", "email"},
-	}
-
-	code := r.URL.Query().Get("code")
-	token, err := config.Exchange(context.Background(), code)
-	if err != nil {
-		http.Error(w, "Failed to exchange code for token", http.StatusInternalServerError)
-		return
-	}
-
-	client := config.Client(context.Background(), token)
-	response, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
-	if err != nil {
-		http.Error(w, "Failed to get user info", http.StatusInternalServerError)
-		return
-	}
-	defer response.Body.Close()
-
-	var userInfo map[string]interface{}
-	err = json.NewDecoder(response.Body).Decode(&userInfo)
-	if err != nil {
-		http.Error(w, "Failed to decode user info", http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Println("userinfo", userInfo["id"], userInfo["email"], userInfo["name"])
 }
