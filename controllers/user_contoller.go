e66d96bec66137e4c5942185f4abe66de4c372df
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-10-10 17:24:15 +0900

    remove: 不要な処理削除

--- controllers/user_contoller.go
+++ controllers/user_contoller.go
@@ -21,7 +21,7 @@ func NewUserController(s services.UserServicer) *UserController {
 	return &UserController{service: s}
 }
 
-func (c *UserController) GoogleLoginHandler(w http.ResponseWriter, r *http.Request) {
+func (c *UserController) GoogleLoginHandler(w http.ResponseWriter, req *http.Request) {
 	config := oauth2.Config{
 		ClientID:     os.Getenv("GOOGLE_CLIANT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIANT_SECRET"),
@@ -30,48 +30,21 @@ func (c *UserController) GoogleLoginHandler(w http.ResponseWriter, r *http.Reque
 		Scopes:       []string{"profile", "email"},
 	}
 	url := config.AuthCodeURL("", oauth2.AccessTypeOffline)
-	http.Redirect(w, r, url, http.StatusFound)
+	http.Redirect(w, req, url, http.StatusFound)
 }
 
-func (c *UserController) GoogleCallbackHandler(w http.ResponseWriter, r *http.Request) {
-	config := oauth2.Config{
-		ClientID:     os.Getenv("GOOGLE_CLIANT_ID"),
-		ClientSecret: os.Getenv("GOOGLE_CLIANT_SECRET"),
-		RedirectURL:  "http://localhost:8080/callback",
-		Endpoint:     google.Endpoint,
-		Scopes:       []string{"profile", "email"},
-	}
-
-	code := r.URL.Query().Get("code")
-	token, err := config.Exchange(context.Background(), code)
-	fmt.Println("token", token)
-	fmt.Println("accesstoken", token.AccessToken)
-	fmt.Println("refreshtoken", token.RefreshToken)
+func (c *UserController) GoogleCallbackHandler(w http.ResponseWriter, req *http.Request) {
+	code := req.URL.Query().Get("code")
+	token, userInfo, err := c.service.GoogleCallbackService(code)
 	if err != nil {
-		http.Error(w, "Failed to exchange code for token", http.StatusInternalServerError)
-		return
-	}
-
-	client := config.Client(context.Background(), token)
-	response, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
-	if err != nil {
-		http.Error(w, "Failed to get user info", http.StatusInternalServerError)
-		return
-	}
-	defer response.Body.Close()
-
-	var userInfo map[string]interface{}
-	err = json.NewDecoder(response.Body).Decode(&userInfo)
-	if err != nil {
-		http.Error(w, "Failed to decode user info", http.StatusInternalServerError)
+		apperrors.ErrorHandler(w, req, err)
 		return
 	}
 
 	fmt.Println("userinfo", userInfo["id"], userInfo["email"], userInfo["name"])
-
 	// DBにユーザー情報格納
 
-	// アクセストークン、リフレッシュトークンを返す
+	json.NewEncoder(w).Encode(token)
 }
 
 func (c *UserController) RegenerateAccessTokenHandler(w http.ResponseWriter, req *http.Request) {
@@ -100,4 +73,5 @@ func (c *UserController) RegenerateAccessTokenHandler(w http.ResponseWriter, req
 	}
 
 	fmt.Println("New Access Token:", newToken)
+
 }
