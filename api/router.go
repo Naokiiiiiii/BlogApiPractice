986870d1d6d5db5e496ac6e882808eb968ac31ff
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-10-10 14:56:45 +0900

    feat: refreshTokenのroute追加

--- api/router.go
+++ api/router.go
@@ -21,6 +21,7 @@ func NewRouter(db *sql.DB) *mux.Router {
 	// 認証が必要ないAPI
 	r.HandleFunc("/login", uCon.GoogleLoginHandler)
 	r.HandleFunc("/callback", uCon.GoogleCallbackHandler)
+	r.HandleFunc("/regenerateToken", uCon.RegenerateAccessTokenHandler)
 
 	// 認証が必要なAPI
 	authRequired := r.PathPrefix("/").Subrouter()
