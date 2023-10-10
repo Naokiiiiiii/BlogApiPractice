2e3eb91e73d9b31939a50956fd43da391978a6b0
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-10-10 14:54:43 +0900

    feat: refreshTokenの処理追加

--- controllers/user_contoller.go
+++ controllers/user_contoller.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Naokiiiiiii/BlogApiPractice/apperrors"
 	"github.com/Naokiiiiiii/BlogApiPractice/controllers/services"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -43,6 +44,9 @@ func (c *UserController) GoogleCallbackHandler(w http.ResponseWriter, r *http.Re
 
 	code := r.URL.Query().Get("code")
 	token, err := config.Exchange(context.Background(), code)
+	fmt.Println("token", token)
+	fmt.Println("accesstoken", token.AccessToken)
+	fmt.Println("refreshtoken", token.RefreshToken)
 	if err != nil {
 		http.Error(w, "Failed to exchange code for token", http.StatusInternalServerError)
 		return
@@ -68,4 +72,32 @@ func (c *UserController) GoogleCallbackHandler(w http.ResponseWriter, r *http.Re
 	// DBにユーザー情報格納
 
 	// アクセストークン、リフレッシュトークンを返す
+}
+
+func (c *UserController) RegenerateAccessTokenHandler(w http.ResponseWriter, req *http.Request) {
+	config := oauth2.Config{
+		ClientID:     os.Getenv("GOOGLE_CLIANT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIANT_SECRET"),
+		RedirectURL:  "http://localhost:8080/callback",
+		Endpoint:     google.Endpoint,
+		Scopes:       []string{"profile", "email"},
+	}
+
+	var refreshToken string
+	if err := json.NewDecoder(req.Body).Decode(&refreshToken); err != nil {
+		err = apperrors.ReqBodyDecodeFailed.Wrap(err, "bad request body")
+		apperrors.ErrorHandler(w, req, err)
+	}
+
+	token := &oauth2.Token{
+		RefreshToken: refreshToken,
+	}
+
+	newToken, err := config.TokenSource(context.Background(), token).Token()
+	if err != nil {
+		fmt.Println("Failed to refresh token:", err)
+		return
+	}
+
+	fmt.Println("New Access Token:", newToken)
 }
