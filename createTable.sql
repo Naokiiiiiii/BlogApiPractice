e9d4a9d1b6a4596f249077b84528f52a903e3681
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-10-11 23:46:20 +0900

    update: db

--- createTable.sql
+++ createTable.sql
@@ -3,7 +3,8 @@ create table if not exists users (
   google_id varchar(255),
   username varchar(255) not null,
   email varchar(255) not null UNIQUE,
-  created_at datetime
+  created_at datetime,
+  updated_at datetime
 );
 
 create table if not exists articles (
@@ -12,6 +13,7 @@ create table if not exists articles (
   contents text not null,
   user_id integer unsigned not null,
   created_at datetime,
+  updated_at datetime,
   foreign key (user_id) references users(user_id)
 );
 
@@ -21,6 +23,7 @@ create table if not exists comments (
   user_id integer unsigned not null,
   message text not null,
   created_at datetime,
+  updated_at datetime,
   foreign key (article_id) references articles(article_id),
   foreign key (user_id) references users(user_id)
 );
@@ -35,14 +38,14 @@ create table if not exists nices (
 );
 
 -- データの挿入
-insert into users (username, email, created_at) values ('naoki', 'exsample@gmail.com', now());
+insert into users (username, email, created_at, updated_at) values ('naoki', 'exsample@gmail.com', now(), now());
 
-insert into articles (title, contents, user_id, created_at) values ('firstPost', 'This is my first blog', 1, now()); 
+insert into articles (title, contents, user_id, created_at, updated_at) values ('firstPost', 'This is my first blog', 1, now(), now()); 
 
-insert into articles (title, contents, user_id, created_at) values ('secondPost', 'This is my second blog', 1, now()); 
+insert into articles (title, contents, user_id, created_at, updated_at) values ('secondPost', 'This is my second blog', 1, now(), now()); 
 
-insert into comments (article_id, user_id, message, created_at) values (1, 1, '1st comment yeah', now());
+insert into comments (article_id, user_id, message, created_at, updated_at) values (1, 1, '1st comment yeah', now(), now());
 
-insert into comments (article_id, user_id, message, created_at) values (1, 1, '2nd comment yeah', now());
+insert into comments (article_id, user_id, message, created_at, updated_at) values (1, 1, '2nd comment yeah', now(), now());
 
 insert into nices (article_id, user_id, created_at) values (1, 1, now());
