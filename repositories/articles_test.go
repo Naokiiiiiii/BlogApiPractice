967f074dbd8dace96a45b89d97fc26e4fdb365e7
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-10-14 10:44:53 +0900

    feat: DeleteArticleテスト作成

--- repositories/articles_test.go
+++ repositories/articles_test.go
@@ -107,3 +107,13 @@ func TestUpdateArticle(t *testing.T) {
 		t.Errorf("new article title is expected %s but got %s\n", article.Title, resultArticle.Title)
 	}
 }
+
+func TestDeleteArticle(t *testing.T) {
+	testArticleID := 1
+
+	err := repositories.DeleteArticle(testDB, testArticleID)
+
+	if err != nil {
+		t.Errorf("DeleteArticle returned an error: %v", err)
+	}
+}
