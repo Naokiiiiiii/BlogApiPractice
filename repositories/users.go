bbc988c18990bd3a1a5ae24270a0dd646418376a
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-10-16 22:56:12 +0900

    feat: ExistUser作成

--- repositories/users.go
+++ repositories/users.go
@@ -6,16 +6,16 @@ import (
 	"github.com/Naokiiiiiii/BlogApiPractice/models"
 )
 
-func InsertUser(db *sql.DB, user models.User) (models.User, error) {
+func InsertUser(db *sql.DB, googleUser models.GoogleUserDataResponse) (models.User, error) {
 
 	const sqlStr = `insert into users (google_id, username, email, created_at) values
 		(?, ?, ?, now());
 	`
 
 	var newUser models.User
-	newUser.GoogleID, newUser.UserName, newUser.Email = user.GoogleID, user.UserName, user.Email
+	newUser.GoogleID, newUser.UserName, newUser.Email = googleUser.Id, googleUser.Name, googleUser.Email
 
-	result, err := db.Exec(sqlStr, user.GoogleID, user.UserName, user.Email)
+	result, err := db.Exec(sqlStr, googleUser.Id, googleUser.Name, googleUser.Email)
 	if err != nil {
 		return models.User{}, err
 	}
@@ -28,7 +28,7 @@ func InsertUser(db *sql.DB, user models.User) (models.User, error) {
 
 func GetUser(db *sql.DB, userID int) (models.User, error) {
 	const sqlStr = `
-		select * from users where user_id = ?;	
+		select * from users where email = ?;	
 	`
 
 	row := db.QueryRow(sqlStr, userID)
@@ -48,4 +48,15 @@ func GetUser(db *sql.DB, userID int) (models.User, error) {
 	}
 
 	return user, nil
+}
+
+func ExistUser(db *sql.DB, googleUser models.GoogleUserDataResponse) error {
+	const sqlStr = `
+		select * from users where email = ?;
+	`
+	var user models.User
+	row := db.QueryRow(sqlStr, googleUser.Email)
+	err := row.Scan(&user.Email, &user.UserID, &user.UserName, &user.GoogleID, &user.CreatedAt, &user.UpdatedAt)
+
+	return err
 }
