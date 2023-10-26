9bb0c7f31080dd111f978e4f92aa07a776847e42
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-10-26 23:11:32 +0900

    feat: bearer認証追加

--- api/middlewares/cors.go
+++ api/middlewares/cors.go
@@ -6,7 +6,7 @@ func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, beaer")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusNoContent)
 			return
