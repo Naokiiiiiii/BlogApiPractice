2f0c76516a82d1070279180313335a8e66726b2c
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-11 15:41:08 +0900

    remove: 不要なコード削除

--- controllers/article_controller.go
+++ controllers/article_controller.go
@@ -32,13 +32,6 @@ func (c *ArticleController) PostArticleHandler(w http.ResponseWriter, req *http.
 		return
 	}
 
-	// authedUserName := common.GetUserName(req.Context())
-	// if reqArticle.UserName != authedUserName {
-	// 	err := apperrors.NotMatchUser.Wrap(errors.New("does not match reqBody user and idtoken user"), "invalid parameter")
-	// 	apperrors.ErrorHandler(w, req, err)
-	// 	return
-	// }
-
 	json.NewEncoder(w).Encode(article)
 }
 
